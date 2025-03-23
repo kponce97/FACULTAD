c9175fa9e6f031a8e1e9d0af8539905dc299a3b1
kevineliass <kevin.ponce@mi.unc.edu.ar>
2025-03-23 01:57:40 -0300

    Ejercicio 0 COMPLETO

--- AyEDII_2025/Lab/lab01/ej0/fixstring.c
+++ AyEDII_2025/Lab/lab01/ej0/fixstring.c
@@ -37,18 +37,16 @@ bool fstring_eq(fixstring s1, fixstring s2)
 
 bool fstring_less_eq(fixstring s1, fixstring s2)
 {
-    bool b = true;
-    unsigned int l1 = fstring_length(s1);
-    unsigned int l2 = fstring_length(s2);
-
-    b = fstring_eq(s1, s2) ? true : false;
-    for (int i = 0; i < l1; i++)
+    bool result;
+    unsigned int len_s1 = fstring_length(s1);
+    unsigned int len_s2 = fstring_length(s2);
+    unsigned int min_length = len_s1 < len_s2 ? len_s1 : len_s2;
+    unsigned int i = 0;
+    while (i < min_length && s1[i] == s2[i])
     {
-        for (int j = 0; j < l2; j++)
-        {
-            b = ?:;
-        }
+        i = i + 1;
     }
+    result = (i == min_length) ? len_s1 <= len_s2 : s1[i] <= s2[i];
 
-    return b;
+    return result;
 }
