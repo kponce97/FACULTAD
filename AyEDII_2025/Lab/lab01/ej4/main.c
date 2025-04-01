d3494e8f101692003eee83380b1bb2726dcf1f83
kevineliass <kevin.ponce@mi.unc.edu.ar>
2025-04-01 17:22:21 -0300

    Ejercicio 4 Completo!

--- AyEDII_2025/Lab/lab01/ej4/main.c
+++ AyEDII_2025/Lab/lab01/ej4/main.c
@@ -11,7 +11,8 @@
 /* Maximum allowed length of the array */
 static const unsigned int MAX_SIZE = 100000u;
 
-void print_help(char *program_name) {
+void print_help(char *program_name)
+{
     /* Print the usage help of this program. */
     printf("Usage: %s <input file path>\n\n"
            "Sort an array given in a file in disk.\n"
@@ -28,11 +29,13 @@ void print_help(char *program_name) {
            program_name);
 }
 
-char *parse_filepath(int argc, char *argv[]) {
+char *parse_filepath(int argc, char *argv[])
+{
     /* Parse the filepath given by command line argument. */
     char *result = NULL;
 
-    if (argc < 2) {
+    if (argc < 2)
+    {
         print_help(argv[0]);
         exit(EXIT_FAILURE);
     }
@@ -42,7 +45,8 @@ char *parse_filepath(int argc, char *argv[]) {
     return result;
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     char *filepath = NULL;
 
     /* parse the filepath given in command line arguments */
@@ -66,28 +70,40 @@ int main(int argc, char *argv[]) {
     /* do the actual sorting */
     selection_sort(copy, length);
 
+    printf("\n-----------------------------------------\n");
     /* show statistics for selection_sort */
     printf("statistics for selection_sort\n");
-    printf("time elapsed=%g,    comparisons: %10u,    swaps: %10u\n", calculate_elapsed_time(), comparisons_number(), swaps_number());
+    printf("\ttime elapsed = %10g,    \n\tcomparisons: %10u,    \n\tswaps: %15u\n", calculate_elapsed_time(), comparisons_number(), swaps_number());
 
-    /* all the same for insertion_sort */
-    /* Usando la idea de las líneas de códigos anteriores
-       muestre las estadísticas (tiempo de ejecución, número de comparaciones e
-       intercambios realizados) para insertion_sort. No te olvides que antes debes
-       copiar el arreglo original, resetear los contadores y setear el tiempo.
-    */
-    /* needs implementation */
+    //! all the same for insertion_sort
 
+    /* reset counters and set time */
+    reset_comparisons_counter();
+    reset_swaps_counter();
+    set_current_time();
 
-    /* all the same for quick_sort */
-    /* Usando la idea de las líneas de códigos anteriores
-       muestre las estadísticas (tiempo de ejecución, número de comparaciones e
-       intercambios realizados) para quick_sort. No te olvides que antes debes
-       copiar el arreglo original, resetear los contadores y setear el tiempo.
-    */
-    /* needs implementation */
+    /* do the actual sorting */
+    insertion_sort(copy, length);
 
+    /* show statistics for insertion_sort */
+    printf("\n-----------------------------------------\n");
+    printf("statistics for insertion_sort\n");
+    printf("\ttime elapsed = %10g,    \n\tcomparisons: %10u,    \n\tswaps: %15u\n", calculate_elapsed_time(), comparisons_number(), swaps_number());
 
+    //! all the same for quick_sort
+
+    /* reset counters and set time */
+    reset_comparisons_counter();
+    reset_swaps_counter();
+    set_current_time();
+
+    /* do the actual sorting */
+    quick_sort(copy, length);
+
+    /* show statistics for quick_sort */
+    printf("\n-----------------------------------------\n");
+    printf("statistics for quick_sort\n");
+    printf("\ttime elapsed = %10g,    \n\tcomparisons: %10u,    \n\tswaps: %15u\n", calculate_elapsed_time(), comparisons_number(), swaps_number());
 
     return EXIT_SUCCESS;
 }
--- AyEDII_2025/Lab/lab01/ej4/sort.c
+++ AyEDII_2025/Lab/lab01/ej4/sort.c
@@ -7,41 +7,92 @@
 #include "sort_helpers.h"
 #include "sort.h"
 
-static unsigned int min_pos_from(int a[], unsigned int i, unsigned int length) {
+static unsigned int min_pos_from(int a[], unsigned int i, unsigned int length)
+{
     unsigned int min_pos = i;
-    for (unsigned int j = i + 1; j < length; ++j) {
-        if (!goes_before(a[min_pos],a[j])) {
+    for (unsigned int j = i + 1; j < length; ++j)
+    {
+        if (!goes_before(a[min_pos], a[j]))
+        {
             min_pos = j;
         }
     }
     return (min_pos);
 }
 
-void selection_sort(int a[], unsigned int length) {
-    for (unsigned int i = 0; i < length; ++i) {
+void selection_sort(int a[], unsigned int length)
+{
+    for (unsigned int i = 0; i < length; ++i)
+    {
         unsigned int min_pos = min_pos_from(a, i, length);
         swap(a, i, min_pos);
     }
 }
 
-
-static void insert(int a[], unsigned int i) {
-    /* copiá acá la implementación que hiciste en el ejercicio 1 */
+static void insert(int a[], unsigned int i, unsigned int length)
+{
+    unsigned int j;
+    j = i;
+    while (j >= 1 && j <= length && goes_before(a[j], a[j - 1]))
+    {
+        swap(a, j - 1, j);
+        printf("swap(a,%d,%d)", j - 1, j);
+        j--;
+    }
 }
 
-void insertion_sort(int a[], unsigned int length) {
-    /* copiá acá la implementación que hiciste en el ejercicio 1 */
+void insertion_sort(int a[], unsigned int length)
+{
+    for (unsigned int i = 1; i < length; ++i)
+    {
+        insert(a, i, length);
+    }
+    assert(array_is_sorted(a, length));
 }
 
+static unsigned int partition(int a[], unsigned int izq, unsigned int der)
+{
+    unsigned int ppiv = izq;
+    unsigned int i = izq + 1;
+    unsigned int j = der;
 
-static unsigned int partition(int a[], unsigned int izq, unsigned int der) {
-    /* copiá acá la implementación que hiciste en el ejercicio 3 */
+    while (i <= j)
+    {
+        if (goes_before(a[ppiv], a[i]) && goes_before(a[j], a[ppiv]))
+        {
+            swap(a, i, j);
+            i++;
+            j--;
+        }
+        else
+        {
+            if (goes_before(a[i], a[ppiv]))
+            {
+                i++;
+            }
+            else if (goes_before(a[ppiv], a[j]))
+            {
+                j--;
+            }
+        }
+    }
+    swap(a, ppiv, j);
+    ppiv = j;
+
+    return ppiv;
 }
 
-static void quick_sort_rec(int a[], unsigned int izq, unsigned int der) {
-    /* copiá acá la implementación que hiciste en el ejercicio 2 */
+static void quick_sort_rec(int a[], unsigned int izq, unsigned int der)
+{
+    if (goes_before(izq, der))
+    {
+        unsigned int pivot = partition(a, izq, der);
+        quick_sort_rec(a, izq, pivot - 1);
+        quick_sort_rec(a, pivot + 1, der);
+    }
 }
 
-void quick_sort(int a[], unsigned int length) {
-    /* copiá acá la implementación que hiciste en el ejercicio 1 */
+void quick_sort(int a[], unsigned int length)
+{
+    quick_sort_rec(a, 0, (length == 0) ? 0 : length - 1);
 }
