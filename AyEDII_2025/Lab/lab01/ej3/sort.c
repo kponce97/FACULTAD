1410e5fab0e5c4cab5e18729a087e15770edc15a
kevineliass <kevin.ponce@mi.unc.edu.ar>
2025-04-01 15:45:46 -0300

    Ejercicio 3 completo!

--- AyEDII_2025/Lab/lab01/ej3/sort.c
+++ AyEDII_2025/Lab/lab01/ej3/sort.c
@@ -6,27 +6,65 @@
 #include "array_helpers.h"
 #include "sort_helpers.h"
 #include "sort.h"
+/*
+proc partition(in/out a: array[1..n] of T, in lft,rgt: nat, out ppiv: nat)
+    var i,j: nat
+    ppiv:= lft
+    i:= lft+1
+    j:= rgt
+    do i <= j -->
+        if  a[i] <= a[ppiv]                     --> i:= i+1
+            a[j] >= a[ppiv]                     --> j:= j-1
+            a[i] >  a[ppiv] ∧ a[j] < a[ppiv]    --> swap(a,i,j)
+        fi
+    od
+    swap(a,ppiv,j)
+    ppiv:= j
+end proc
+ */
+static unsigned int partition(int a[], unsigned int izq, unsigned int der)
+{
+    unsigned int ppiv = izq;
+    unsigned int i = izq + 1;
+    unsigned int j = der;
 
+    while (i <= j)
+    {
+        if (goes_before(a[ppiv], a[i]) && goes_before(a[j], a[ppiv]))
+        {
+            swap(a, i, j);
+            i++;
+            j--;
+        }
+        else
+        {
+            if (goes_before(a[i], a[ppiv]))
+            {
+                i++;
+            }
+            else if (goes_before(a[ppiv], a[j]))
+            {
+                j--;
+            }
+        }
+    }
+    swap(a, ppiv, j);
+    ppiv = j;
 
-static unsigned int partition(int a[], unsigned int izq, unsigned int der) {
-    /* Needs implementation */
-
-    /* PRECONDITION: 
-       0 <= izq < der < length of the array
-
-     Permutes elements of a[izq..der] and returns pivot such that:
-     - izq <= pivot <= der
-     - elements in a[izq,pivot) all 'go_before' (according to function goes_before) a[pivot]
-     - a[pivot] 'goes_before' all the elements in a(pivot,der]
-    */
+    return ppiv;
 }
 
-static void quick_sort_rec(int a[], unsigned int izq, unsigned int der) {
-    /* copiá acá la implementación que hiciste en el ejercicio 3 */
-
+static void quick_sort_rec(int a[], unsigned int izq, unsigned int der)
+{
+    if (goes_before(izq, der))
+    {
+        unsigned int pivot = partition(a, izq, der);
+        quick_sort_rec(a, izq, pivot - 1);
+        quick_sort_rec(a, pivot + 1, der);
+    }
 }
 
-void quick_sort(int a[], unsigned int length) {
+void quick_sort(int a[], unsigned int length)
+{
     quick_sort_rec(a, 0, (length == 0) ? 0 : length - 1);
 }
-
